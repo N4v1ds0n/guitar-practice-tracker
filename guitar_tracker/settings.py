32924963c125e83c75c684c9af4725e87aa94cfe
N4v1ds0n <damian.droste@gmx.de>
2025-06-17 23:08:58 +0200

    Add Static root to fix builld issue

--- guitar_tracker/settings.py
+++ guitar_tracker/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0*(eg)2h27eu=ykeww4590&s+%baksrn!n7p9d)cju8scuyh4c' # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -134,6 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
